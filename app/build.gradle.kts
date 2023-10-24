7d771ae4f753b558ad5c42dbb27103f5d1073bf8
Kelvin Ricafort <kelvs.r@outlook.com>
2023-10-24 16:29:45 +0800

    Sending Data to Activity

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,12 +4,12 @@ plugins {
 
 android {
     namespace = "com.newtechieblog.wordpress.views.sendingdata"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.newtechieblog.wordpress.views.sendingdata"
         minSdk = 24
-        targetSdk = 33
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
--- app/src/main/java/com/newtechieblog/wordpress/views/sendingdata/MainActivity.java
+++ app/src/main/java/com/newtechieblog/wordpress/views/sendingdata/MainActivity.java
@@ -2,13 +2,43 @@ package com.newtechieblog.wordpress.views.sendingdata;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText editTextName, editTextEmail, editTextPhone;
+    Button btnSignup;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        editTextName = findViewById(R.id.editTextName);
+        editTextEmail = findViewById(R.id.editTextEmail);
+        editTextPhone = findViewById(R.id.editTextPhone);
+
+        btnSignup = findViewById(R.id.btnSignup);
+
+        btnSignup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String userName = editTextName.getText().toString();
+                String userEmail = editTextEmail.getText().toString();
+                int userPhone = Integer.parseInt(editTextPhone.getText().toString());
+
+                Intent intent = new Intent(getApplicationContext(), SecondActivity.class);
+                intent.putExtra("name", userName);
+                intent.putExtra("email", userEmail);
+                intent.putExtra("phone", userPhone);
+
+                startActivity(intent);
+            }
+        });
     }
 }
